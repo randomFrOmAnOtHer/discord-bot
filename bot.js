a16a03eb5afd019b6d6097fe2657955d26f79f32
randomFrOmAnOtHer <40089614+randomFrOmAnOtHer@users.noreply.github.com>
2018-06-09 10:58:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.user.setGame('with ur mommy')
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
