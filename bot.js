05c7d2bc56402f0d11a6bf343181285125430208
randomFrOmAnOtHer <40089614+randomFrOmAnOtHer@users.noreply.github.com>
2018-06-09 10:01:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,19 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+var prefix = "!"
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'ping')) {
+		message.channel.sendMessage('Pong');
+	}
+});
+
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'invite')) {
+		message.channel.sendMessage('https://discordapp.com/oauth2/authorize/?permissions=2146958591&scope=bot&client_id=454681676929564672');
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
